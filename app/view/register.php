be422dae1d2f87b8480569b29f162e0c5ce29363
v18000341 <aurelien.vilminot@etu.univ-amu.fr>
2019-10-03 14:32:40 +0200

    Register View

--- app/view/register.php
+++ app/view/register.php
@@ -1,15 +1,15 @@
-<form action="verifReg.php" method="POST">//Formulaire d'inscription
-    <label><b>Nom d'utilisateur</b></label>
-    <input type="text" placeholder="Entrer un nom d\'utilisateur" name="Username" required>
+<form action="" method="POST">
+    <label>Nom d'utilisateur</label>
+    <input type="text" placeholder="Entrer un nom d'utilisateur" name="login" required>
 
-    <label><b>E-mail</b></label>
+    <label>E-mail</label>
     <input type="text" placeholder="Entrer un e-mail" name="mail" required>
 
-    <label><b>Mot de passe</b></label>
+    <label>Mot de passe<</label>
     <input type="password" placeholder="Entrer un mot de passe" name="password" required>
 
-    <label><b>Vérification du mot de passe</b></label>
+    <label>Vérification du mot de passe</label>
     <input type="password" placeholder="Vérifier le mot de passe" name="check_password" required>
 
-    <input type="submit" id="submit" name="action" value="Inscription">
+    <input type="submit" id="submit" name="register" value="Inscription">
 </form>
