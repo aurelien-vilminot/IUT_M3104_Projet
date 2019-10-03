8662dc2b16e027d6eefea259337d5ea9f9cf0a2b
v18000341 <aurelien.vilminot@etu.univ-amu.fr>
2019-10-03 15:27:51 +0200

    Register

--- app/model/register.php
+++ app/model/register.php
@@ -2,7 +2,7 @@
 
 
     function email_taken($email){
-        include '../app/model/DB_Connection';
+        include '../app/model/DB_Connection.php';
 
         $tab = array('email' => $email);
         $sql = 'SELECT * FROM USER WHERE MAIL = :email';
@@ -14,7 +14,7 @@
     }
 
     function login_taken($login){
-        include '../app/model/DB_Connection';
+        include '../app/model/DB_Connection.php';
 
         global $DataBase;
         $tab = array('login' => $login);
@@ -27,7 +27,7 @@
     }
 
     function register($login, $email, $password, $isAdmin){
-        include '../app/model/DB_Connection';
+        include '../app/model/DB_Connection.php';
 
         $tab = array('login'=>$login,'email'=>$email,'password'=>$password,'isAdmin'=>$isAdmin);
         $sql = 'INSERT INTO USER (LOGIN,MAIL,PASSWORD,ADMIN) VALUES(:login,:email,:password,:isAdmin)';
