3a122ad16448999bc9cae4ec7db8e8019b9eec96
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-10-03 14:05:58 +0200

    Formulaire d'inscription (en cours)

--- app/control/form.php
+++ app/control/form.php
@@ -1,1 +1,19 @@
 <?php
+
+include '../model/register.php';
+
+$Id = $_POST['id'];
+$Mail = $_POST['mail'];
+$Password = $_POST['password'];
+$check_password = $_POST['check_password'];
+$action = $_POST['action'];
+
+if ($action == 'Inscription')
+{
+    $message = 'Voici vos identifiants d\'inscription:' .PHP_EOL;
+    $message .= 'Email:' . register::getId() . PHP_EOL;
+    $message .= 'Mot de passe:' . PHP_EOL . register::getPassword();
+    $header = "From:". register::getMail();
+    $header .= "Reply-to:".getMail();
+    $header .= 'Content-type: text/html';
+}
--- app/control/register.php
+++ app/control/register.php
@@ -1,1 +1,19 @@
-<?='inscription'?>
+<?php
+
+include '../model/register.php';
+
+$Id = $_POST['id'];
+$Mail = $_POST['mail'];
+$Password = $_POST['password'];
+$check_password = $_POST['check_password'];
+$action = $_POST['action'];
+
+if ($action == 'Inscription')
+{
+    $message = 'Voici vos identifiants d\'inscription:' .PHP_EOL;
+    $message .= 'Email:' . register::getId() . PHP_EOL;
+    $message .= 'Mot de passe:' . PHP_EOL . register::getPassword();
+    $header = "From:". register::getMail();
+    $header .= "Reply-to:".register::getMail();
+    $header .= 'Content-type: text/html';
+}
