a6e360e11b39e429aabd555bfa132186c0e246ea
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-03 11:32:37 +0200

    Modèle MVC

---
+++ app/view/menu.php
@@ -0,0 +1,11 @@
+<section class="menu">
+    <div class="topMenuBorder"></div>
+    <div class="imgMenu">
+        <img src="img/logo.png">
+    </div>
+    <div class="middleMenuBorder"></div>
+    <div class="pages">
+        <a href="index.php?page=login">Connexion</a>
+        <a href="index.php?page=register">Inscription</a>
+    </div>
+</section>
--- www/index.php
+++ www/index.php
@@ -1,5 +1,32 @@
 <?php
-
-    include '../app/control/test.php';
-
+    if(isset($_GET['page']) && !empty($_GET['page']))
+        $page = $_GET['page'];
+    else
+        $page = 'home';
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="FreeNote - Réseau Social">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FreeNote</title>
+    <link rel="stylesheet" id="style" href="style/index.css" />
+    <link rel="icon" type="image/png" href=""/>
+</head>
+<body>
+    <?php include '../app/view/menu.php'?>
+    <main>
+        <div class="topMainBorder"></div>
+
+        <section class="data">
+            <h1>FreeNote</h1>
+            <?php
+                include '../app/control/' . $page . '.php';
+            ?>
+        </section>
+
+        <?php include '../app/view/footer.php'?>
+    </main>
+</body>
+</html>
