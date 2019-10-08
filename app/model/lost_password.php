78e3243ae94648de9492bf7ad00a3d0c6fda21db
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-08 22:44:04 +0200

    Système de récupération de mot de passe

--- app/model/lost_password.php
+++ app/model/lost_password.php
@@ -54,10 +54,10 @@
         public function sendMail()
         {
             $message = 'Bonjour ' . $this->login . ', ' . "\n" . "\n";
-            $message .= 'Vous avez fait une demande de réinitialisation de votre mot de passe. Voici donc vos identifiants tempraires : ' . "\n";
+            $message .= 'Vous avez fait une demande de réinitialisation de votre mot de passe. Voici vos identifiants tempraires : ' . "\n";
             $message .= 'Identifiant : ' . $this->login . "\n";
             $message .= 'Mot de passe : ' . $this->setNewPassword() . "\n" . "\n";
-            $message .= 'Pour modifier votre mot de passe, aller sur votre profil puis saissez votre nouveau mot de passe.' . "\n" . "\n";
+            $message .= 'Pour modifier votre mot de passe, allez sur votre profil puis saisissez votre nouveau mot de passe.' . "\n" . "\n";
             $message .= 'L\'équipe FreeNote.';
 
             $header = 'From: FreeNote <reset.password@freenote.fr>' . "\n";
