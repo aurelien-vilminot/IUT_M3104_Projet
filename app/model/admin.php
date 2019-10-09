6426b50a4e9a9a120e96ff0e5a82c4f05f5f3fd7
hsiacelia <celia.hsia@etu.univ-amu.fr>
2019-10-09 16:45:52 +0200

    structure

---
+++ app/model/admin.php
@@ -0,0 +1,82 @@
+<?php
+include 'user.php';
+
+class Admin extend User {
+
+  public _construct () {};
+
+  public function open_discussion()
+  {
+
+  }
+
+  public function close_discussion()
+  {
+
+  }
+
+  public function create_user ($user)
+  {
+    new User;
+  }
+
+  public function read_user ($user)
+  {
+
+  }
+
+  public function  update_user ($user)
+  {
+
+  }
+
+  public function delete_user ($user)
+  {
+
+  }
+
+
+  public function create_discussion ($discussion)
+  {
+
+  }
+
+  public function  read_discussion ($discussion)
+  {
+
+  }
+
+  public function  update_discussion ($discussion)
+  {
+
+  }
+
+  public function delete_discussion ($discussion)
+  {
+
+  }
+
+  public function create_message ($message)
+  {
+
+  }
+
+  public function read_message ($message)
+  {
+
+  }
+
+  public function update_message ($message)
+  {
+
+  }
+
+  public function delete_message ($message)
+  {
+
+  }
+
+}
+
+
+?>
