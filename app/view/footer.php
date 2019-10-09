5ef4ee88698fdc02d4b39c9da24a9cd87fc22ed6
b18015277 <tom.bertolone@etu.univ-amu.fr>
2019-10-09 16:50:42 +0200

    ajout contenu du footer

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,4 +1,8 @@
 <footer>
-    <a href="https://www.atmosud.org/" target="_blank"><img src="img/atmosud.png"></a>
-    <a href="https://lce.univ-amu.fr/" target="_blank"><img src="img/lce.png"></a>
+    <p>FreeNote | Copyright 2019 ©</p><br>
+    <p> Github : </p>
+    <a href="https://github.com/aurelien-vilminot" >Aurélien V</a>
+    <a href="https://github.com/Tom-Bertolone" >Tom Bertolone</a>
+    <a href="https://github.com/hsiacelia" >Célia Hsia</a>
+    <a href="https://github.com/Samy4513" >Samy Kamboui</a>
 </footer>
