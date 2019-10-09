0896c23cc872463072ef9493bdcea2a3c44269e3
b18015277 <tom.bertolone@etu.univ-amu.fr>
2019-10-09 16:52:18 +0200

    ajout contenu du footer

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <p>FreeNote | Copyright 2019 ©</p><br>
-    <p> Github : </p>
-    <a href="https://github.com/aurelien-vilminot" >Aurélien V</a>
-    <a href="https://github.com/Tom-Bertolone" >Tom Bertolone</a>
-    <a href="https://github.com/hsiacelia" >Célia Hsia</a>
-    <a href="https://github.com/Samy4513" >Samy Kamboui</a>
+<p>FreeNote | Copyright 2019 ©<br>
+Github : </p>
+<a href="https://github.com/aurelien-vilminot" >Aurélien Vilminot</a>
+<a href="https://github.com/Tom-Bertolone" >Tom Bertolone</a>
+<a href="https://github.com/hsiacelia" >Célia Hsia</a>
+<a href="https://github.com/Samy4513" >Samy Kamboui</a>
 </footer>
