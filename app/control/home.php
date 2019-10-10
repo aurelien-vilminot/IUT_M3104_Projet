240bb01cecde30c18e5bfee91bb21ef2a622b53a
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-10 14:40:59 +0200

    Class discussion

--- app/control/home.php
+++ app/control/home.php
@@ -1,8 +1,8 @@
 <?php
     if(isset($_SESSION['user']) && $_SESSION['user'] == 1)
     {
-        require_once '../app/model/user.php';
-        require_once '../app/model/discussions.php';
+        //require_once '../app/model/user.php';
+        //require_once '../app/model/discussions.php';
 
 //        $myUser = new User($_SESSION['loginCurrentUser']);
 //        $id = $myUser->getLogin();
