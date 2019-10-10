acc40c3209d14115d0889ae0a476758d69ac02ad
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-10 11:34:28 +0200

    Contrôleur profil

--- app/control/profil.php
+++ app/control/profil.php
@@ -8,4 +8,38 @@
     $id = $myUser->getLogin();
     $mail = $myUser->getMail();
 
+    if(isset($_POST['submit_mail']))
+    {
+        $newMail = trim($_POST['mail']);
+
+        if($myUser->email_taken($mail) == 1)
+        {
+            $error_email = 'L\'adresse email est déjà utilisée';
+        }
+        else
+        {
+            $myUser->setMail($newMail);
+        }
+    }
+
+    if(isset($_POST['submit_password']))
+    {
+        $password = trim($_POST['old_password']);
+        $newPassword = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
+        $newCheckPassword = trim($_POST['check_password']);
+
+        if (!password_verify($password, $myUser->getPassword()))
+        {
+            $error_login = 'L\' ancien mot de passe est erroné';
+        }
+        else if (!password_verify($newCheckPassword, $newPassword))
+        {
+            $error_password = 'Les mots de passe ne correspondent pas';
+        }
+        else
+        {
+            $myUser->setPassword($newPassword);
+        }
+    }
+
     require '../app/view/profil.php';
--- app/view/profil.php
+++ app/view/profil.php
@@ -8,14 +8,31 @@
             <h3>Modification de votre Mail :<br></h3>
             <form action="" method="post">
                 <input type="text" placeholder="Entrez une nouvelle adresse mail" name="mail" required>
+                <?php
+                if (isset($error_email))
+                    echo <<<EOT
+<p class="error">$error_email</p>
+EOT;
+                ?>
                 <input type="submit" name="submit_mail" value="Modifier">
             </form>
         </div>
         <div class="modifPwd">
             <h3>Modification de votre mot de passe :</h3>
             <form action="" method="post">
+                <input type="password" placeholder="Entrez votre mot de passe actuel" name="old_password" required>
                 <input type="password" placeholder="Entrez un nouveau mot de passe" name="password" required>
                 <input type="password" placeholder="Verifiez le nouveau mot de passe" name="check_password" required>
+                <?php
+                if (isset($error_login))
+                    echo <<<EOT
+<p class="error">$error_login</p>
+EOT;
+                elseif (isset($error_password))
+                    echo <<<EOT
+<p class="error">$error_password</p>
+EOT;
+                ?>
                 <input type="submit" name="submit_password" value="Modifier">
             </form>
         </div>
