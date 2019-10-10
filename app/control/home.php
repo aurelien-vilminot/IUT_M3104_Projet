6a329f4fffbc2fda85c9ff666346e58309a5546f
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-10 12:02:46 +0200

    Home

--- app/control/home.php
+++ app/control/home.php
@@ -1,5 +1,8 @@
 <?php
-if(isset($_SESSION['user']) && $_SESSION['user'] == 1)
-{
-    echo 'tu es connecté !';
-}
+    if(isset($_SESSION['user']) && $_SESSION['user'] == 1)
+    {
+        echo 'tu es connecté !';
+    }
+
+    require '../app/view/home.php';
+
---
+++ app/view/home.php
@@ -0,0 +1,20 @@
+<div class="home">
+    <div>
+        <p>
+            Bienvenue sur FreeNote
+            <?php
+            if (isset($id))
+                echo <<<EOT
+ $id
+EOT;
+            ?>
+            ,<br><br>
+            Ce réseau social à pour vocation d'être inutile... Mais bon on a pas le choix de le faire, désolé les gars :(<br><br>
+            Plus sérieusement, réseau social d’un nouveau genre, FreeNote consiste à créer des fils de discussions comprenant des messages participatifs au sein desquels chaque utilisateur ne peut ajouter qu’un ou deux mots.
+        </p>
+    </div>
+
+    <div>
+        <p>Discussions (à venir)</p>
+    </div>
+</div>
