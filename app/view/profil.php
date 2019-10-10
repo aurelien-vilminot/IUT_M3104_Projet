814747f6996e730da5a407f350aa32af500b8e13
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-10 11:09:27 +0200

    Vue profil

--- app/view/profil.php
+++ app/view/profil.php
@@ -3,16 +3,22 @@
         Voici vos informations : <br><br>
         Mail : <?=$mail?><br<br><p>
 
-    <h3>Modification de votre Mail :<br></h3>
-    <form action="" method="post">
-        <input type="text" placeholder="Entrez une nouvelle adresse mail" name="mail" required>
-        <input type="submit" name="submit_mail" value="Modifier">
-    </form>
+    <div class="modif">
+        <div class="modifMail">
+            <h3>Modification de votre Mail :<br></h3>
+            <form action="" method="post">
+                <input type="text" placeholder="Entrez une nouvelle adresse mail" name="mail" required>
+                <input type="submit" name="submit_mail" value="Modifier">
+            </form>
+        </div>
+        <div class="modifPwd">
+            <h3>Modification de votre mot de passe :</h3>
+            <form action="" method="post">
+                <input type="password" placeholder="Entrez un nouveau mot de passe" name="password" required>
+                <input type="password" placeholder="Verifiez le nouveau mot de passe" name="check_password" required>
+                <input type="submit" name="submit_password" value="Modifier">
+            </form>
+        </div>
+    </div>
 
-    <h3>Modification de votre mot de passe :</h3>
-    <form action="" method="post">
-        <input type="password" placeholder="Entrez un nouveau mot de passe" name="password" required>
-        <input type="password" placeholder="Verifiez le nouveau mot de passe" name="check_password" required>
-        <input type="submit" name="submit_password" value="Modifier">
-    </form>
 </div>
