b3cc2031ad34530ba83a7099980d85f171def0ae
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-10 10:52:12 +0200

    Controleur profil

--- app/control/login.php
+++ app/control/login.php
@@ -14,6 +14,7 @@
         if($loginUser->verif_user($login, $password) == 0)
         {
             $_SESSION['user'] = 1;
+            $_SESSION['loginCurrentUser'] = $login;
             header('Location: index.php?page=home');
         }
         elseif ($loginUser->verif_user($login, $password) == 2)
--- app/control/profil.php
+++ app/control/profil.php
@@ -2,5 +2,7 @@
     if(!isset($_SESSION['user']) && $_SESSION['user'] != 1)
         header("Location: index.php?page=login");
 
-    $test = new User('aurelien');
+    require_once '../app/model/user.php';
+
+    $test = new User($_SESSION['loginCurrentUser']);
     echo $test->getMail();
