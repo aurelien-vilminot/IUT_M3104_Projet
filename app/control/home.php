8c54d51370c494af5e3be30a2007d4e987581e7a
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-13 14:52:28 +0200

    Discussion home

--- app/control/home.php
+++ app/control/home.php
@@ -2,7 +2,6 @@
 
     require_once '../app/model/discussions.php';
     require '../app/model/user.php';
-
     $myDiscussions = new Discussion();
     $myDiscussion = new User('aurelien1');
     $tabDisc = $myDiscussions->getAllDiscussion();
