7919a2d8c20bfba42084df82bcc072b32bb14259
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-13 16:40:39 +0200

    Discussion home

--- app/control/home.php
+++ app/control/home.php
@@ -3,9 +3,9 @@
     require_once '../app/model/home.php';
     $myHome = new Home();
     $tabDisc = $myHome->getAllDiscussion();
-    echo $tabDisc[0][0] . $tabDisc[0][1];
-    echo $tabDisc[1][0] . $tabDisc[1][1];
-    echo $tabDisc[2][0] . $tabDisc[2][1];
+    echo $tabDisc[0][0] . ' ' . $tabDisc[0][1] .PHP_EOL;
+    echo $tabDisc[1][0] . ' ' . $tabDisc[1][1] .PHP_EOL;
+    echo $tabDisc[2][0] . ' ' . $tabDisc[2][1] .PHP_EOL;
 
     require '../app/view/home.php';
 
