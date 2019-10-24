a76a1650dd2021cd4c26db4d6383bc70544fe539
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2019-10-24 16:56:39 +0200

    URL Rewriting

--- app/view/layouts/menu.php
+++ app/view/layouts/menu.php
@@ -4,11 +4,11 @@
         <?php
         if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1)
             echo <<<EOT
-<a href="index.php"><img alt="Logo" src="media/logo_admin.png"></a>
+<a href="home"><img alt="Logo" src="media/logo_admin.png"></a>
 EOT;
         else
             echo <<<EOT
-<a href="index.php"><img alt="Logo" src="media/logo.png"></a>
+<a href="home"><img alt="Logo" src="media/logo.png"></a>
 EOT;
         ?>
     </div>
@@ -18,16 +18,16 @@ EOT;
             if(isset($_SESSION['CurrentUser']))
             {
                 echo <<<EOT
-<a href="index.php?page=profil">Mon profil</a>
+<a href="profil">Mon profil</a>
 EOT;
                 if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1)
                     echo <<<EOT
-<a href="index.php?page=users">Liste des utilisateurs</a>
-<a href="index.php?page=settings">Paramètres du site</a>
-<a href="index.php?page=setDatabase">Administration base de données</a>
+<a href="users">Liste des utilisateurs</a>
+<a href="settings">Paramètres du site</a>
+<a href="setDatabase">Administration base de données</a>
 EOT;
                 echo <<<EOT
-<a href="index.php?page=logout">Déconnexion</a>
+<a href="logout">Déconnexion</a>
 EOT;
             }
             else
