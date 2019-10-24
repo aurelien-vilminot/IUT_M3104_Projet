e20cbb5debc77c8ae44753e06ae6c1fe086eeeb6
Tom Bertolone <49900647+Tom-Bertolone@users.noreply.github.com>
2019-10-24 16:51:56 +0200

    Comment

--- app/model/settings.php
+++ app/model/settings.php
@@ -1,14 +1,14 @@
 <?php
     class settings extends database
     {
-        public function setJSONFile($text)
+        public function setJSONFile($text)      //Récupère le fichier JSON
         {
             $myfile = fopen('../app/files/config.json', "w");
             fwrite($myfile, $text);
             fclose($myfile);
         }
 
-        public function JSONtoString()
+        public function JSONtoString()          //Traduit le fichier JSON en chaine de caractère
         {
             return file_get_contents('../app/files/config.json');
         }
