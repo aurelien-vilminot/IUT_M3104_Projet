34d7c36875ce133578a3b9fc426ded37da3f7613
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2020-04-19 11:47:44 +0200

    update README.md

--- app/view/layouts/footer.php
+++ app/view/layouts/footer.php
@@ -2,8 +2,5 @@
             <p>Copyright © 2019 | FreeNote</p>
             <div class="github">
                 <a href="https://github.com/aurelien-vilminot" target="_blank">Aurélien VILMINOT</a>
-                <a href="https://github.com/Tom-Bertolone" target="_blank" >Tom BERTOLONE</a>
-                <a href="https://github.com/hsiacelia" target="_blank" >Célia HSIA</a>
-                <a href="https://github.com/Samy4513" target="_blank" >Samy KAMBOUI</a>
             </div>
         </footer>
